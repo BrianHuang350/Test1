ad211ab22507a36f2ac6fccaa581499da278795c
Brian Huang <ace980218@gmail.com>
2024-09-12 22:25:45 +0800

    add middleware

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
