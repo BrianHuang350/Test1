4c4ad180a7832363d55448e95116e284de882a0c
Brian Huang <ace980218@gmail.com>
2024-09-12 22:17:58 +0800

    commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/test", (req, res) => {
-  return res.json({
-    message: "成功部署",
-  });
+  res.render("Success");
 });
 
 app.listen(8080, () => {
