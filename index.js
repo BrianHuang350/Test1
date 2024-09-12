2be9f158fd6c6420fa731be7c2532967e19e0694
Brian Huang <ace980218@gmail.com>
2024-09-12 23:04:53 +0800

    add script

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+// app.set("views", path.join(__dirname, "/views"));
 
 app.get("/test", (req, res) => {
   res.render("Success");
